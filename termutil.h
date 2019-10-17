ddbd7386c621390c20a5b8eb87c8b274e6733401
Lily Hopkins <bsalarius@gmail.com>
2019-10-17 12:10:05 +0100

    0.1.1: Swapped tabs for speaced in header file

--- termutil.h
+++ termutil.h
@@ -1,8 +1,8 @@
 #pragma once
 
 extern "C" {
-	void tu_pause();
-	void tu_pause_with_message(const char *msg);
-	void tu_clear();
-	void tu_sleep(unsigned int millis);
+  void tu_pause();
+  void tu_pause_with_message(const char *msg);
+  void tu_clear();
+  void tu_sleep(unsigned int millis);
 }
